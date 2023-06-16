485337f09311efd3038138fd1d95a47123c5f376
Cristian Santander <casdesarrollo.2022@gmail.com>
2023-06-16 18:59:47 -0300

    solucionado html nuevaPersona

--- src/main/java/com/example/ejemplos_video/entities/UserRole.java
+++ src/main/java/com/example/ejemplos_video/entities/UserRole.java
@@ -15,12 +15,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
 @Table(name="user_role", uniqueConstraints = @UniqueConstraint(columnNames= {"role", "user_id"}))
 public class UserRole {
 	
